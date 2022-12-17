0ef22303f30eebf026c5c617d1b4d71fc87c58a4
語萱 簡 <410570011@m365.fju.edu.tw>
2022-12-17 14:34:19 +0800

    歡迎介面 完成

---
+++ app/src/main/java/com/example/money/WelcomeActivity.kt
@@ -0,0 +1,36 @@
+package com.example.money
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+
+class WelcomeActivity : Activity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome)
+        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 2000) //2秒跳轉
+    }
+
+    private val mHandler: Handler = @SuppressLint("HandlerLeak")
+    object : Handler() {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                GOTO_MAIN_ACTIVITY -> {
+                    val intent = Intent()
+                    //將原本Activity的換成MainActivity
+                    intent.setClass(this@WelcomeActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else -> {}
+            }
+        }
+    }
+
+    companion object {
+        private const val GOTO_MAIN_ACTIVITY = 0
+    }
+}
